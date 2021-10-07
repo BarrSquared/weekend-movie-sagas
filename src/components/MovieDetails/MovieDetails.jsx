92d143c238dc1c4d1495d58cdd44e571fb11f4b7
Russell Barr <rbarrsquared@gmail.com>
2021-10-06 20:25:26 -0500

    created MovieDetails component

---
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -0,0 +1,15 @@
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+function MovieDetails() {
+    
+
+
+    return(
+        <>
+
+        </>
+    )
+}
+
+export default MovieDetails;
