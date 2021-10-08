83be251a912eb8acaacc766b7c360d2e595d0561
Russell Barr <rbarrsquared@gmail.com>
2021-10-08 00:29:58 -0500

    addMovie drop down llist wofunctional

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { useState } from 'react';
+import MovieDetails from '../MovieDetails/MovieDetails';
 
 
 
@@ -12,7 +13,7 @@ function AddMovie() {
     const genreList = useSelector(store => store.genres)
 
     const [ newMovie, setNewMovie ] = useState({
-        title: '', poster: '', description: '', genres: []
+        title: '', poster: '', description: '', genres: '',
     });
 
     function fetchGenres() {
@@ -36,24 +37,13 @@ function AddMovie() {
         }
     }
 
-    const genreArray = (event) => {
-        const checkId = event.target.value;
-        if(checkId == 'MENU') {
-            return;
-        }
-
-        for (let id of newMovie.genres) {
-            if(id == checkId) {
-                return;
-            }
-        }
-        setNewMovie({...newMovie, genres: [...newMovie.genres, event.target.value]})
-    }
-
     const handleCancel = () => {
         history.push("/");
     }    
 
+    const setGenre = (event) => {
+        setNewMovie({...newMovie, genres: event.target.value});
+    }
     return(
         <>
             <h2>Add a Movie</h2>
@@ -67,29 +57,35 @@ function AddMovie() {
                     setNewMovie({...newMovie, title: event.target.value})}
                 />
                 <input 
-                required
-                type='text'
-                placeholder='Description'
-                value={newMovie.description}
-                onChange={ (event) => 
-                setNewMovie({...newMovie, description: event.target.value}) }
+                    required
+                    type='text'
+                    placeholder='Description'
+                    value={newMovie.description}
+                    onChange={ (event) => 
+                    setNewMovie({...newMovie, description: event.target.value}) }
                 />
                 <input
-                required
-                type='text'
-                placeholder='Url'
-                value={newMovie.poster}
-                onChange={ (event) => 
-                setNewMovie({...newMovie, poster: event.target.value})}
+                    required
+                    type='text'
+                    placeholder='Url'
+                    value={newMovie.poster}
+                    onChange={ (event) => 
+                    setNewMovie({...newMovie, poster: event.target.value})}
                 />
-                <select 
-                required 
-                label="Select Genres" 
-                onChange={(event) => genreArray(event)}>
-                    <option value='MENU'>Genre</option>
-                    {genreList.map((genre) =>{
-                        return(<option key={genre.id} value={genre.id}>{genre.name}</option>);
-                    })}
+                <select selected value={genreList} onChange={setGenre}>
+                    <option value={1}>Adventure</option>
+                    <option value={2}>Animated</option>
+                    <option value={3}>Biographical</option>
+                    <option value={4}>Comedy</option>
+                    <option value={5}>Disaster</option>
+                    <option value={6}>Drama</option>
+                    <option value={7}>Epic</option>
+                    <option value={8}>Fantasy</option>
+                    <option value={9}>Musical</option>
+                    <option value={10}>Romantic</option>
+                    <option value={11}>Science Fiction</option>
+                    <option value={12}>Space-Opera</option>
+                    <option value={13}>Superhero</option>
                 </select>
                 <button onClick={handleCancel}>Cancel</button>
                 <button type="submit">Save</button>    
@@ -100,13 +96,13 @@ function AddMovie() {
 
 export default AddMovie;
 
-// - [] an input field (for the movie title)
-// - [] an input field (for the movie poster image URL))
-// - [] a textarea (for the movie description)
+// - [X] an input field (for the movie title)
+// - [X] an input field (for the movie poster image URL))
+// - [X] a textarea (for the movie description)
 // - [] a dropdown (for the genres)
 
 // The Add Movie page should have the buttons:
 
-// - [] `Cancel` button, which should bring the user to the Home/List Page
+// - [X] `Cancel` button, which should bring the user to the Home/List Page
 // - [] `Save` button, which should save these inputs in the database and 
 // bring the user to the Home/List Page (which now has the new movie)
